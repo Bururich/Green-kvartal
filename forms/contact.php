e0c256467724b99a194d3fe257a9cf85394e1477
Bururich <rgubrij@gmail.com>
2025-09-17 02:16:22 +0200

    diff changes

--- forms/contact.php
+++ forms/contact.php
@@ -1,42 +1,29 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// Укажи email, на который будут приходить письма
+$to = "rgubrij@gmail.com"; // ← замени на свой
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Получаем данные из формы
+$name    = $_POST['name'] ?? '';
+$email   = $_POST['email'] ?? '';
+$phone   = $_POST['phone'] ?? '';
+$subject = $_POST['subject'] ?? '';
+$message = $_POST['message'] ?? '';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Формируем текст письма
+$email_subject = "Нове повідомлення: $subject";
+$email_body = "Ім'я: $name\n";
+$email_body .= "Email: $email\n";
+$email_body .= "Телефон: $phone\n";
+$email_body .= "Повідомлення:\n$message\n";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Заголовки
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  isset($_POST['phone']) && $contact->add_message($_POST['phone'], 'Phone');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+// Отправка письма
+if (mail($to, $email_subject, $email_body, $headers)) {
+  echo "success";
+} else {
+  echo "error";
+}
 ?>
